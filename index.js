e039c80bba10d40a4fa2fde722368089ca358614
EmIT <emad.armoun@gmail.com>
2021-10-30 17:15:15 +0330

    fix: solve multiply problem

--- index.js
+++ index.js
@@ -21,58 +21,66 @@ module.exports = function(num) {
       return str.substr(zeroLength);
   }
 
-  HugeNum.prototype.sum = function(b) {
-      let result = '';
+  function sum(a, b) {
+    let result = '';
 
-      const aRevStr = strReverse(this.value);
-      const bRevStr = strReverse(b);
+    const aRevStr = strReverse(a);
+    const bRevStr = strReverse(b);
+    let added = 0;
+    const maxSize = Math.max(aRevStr.length, bRevStr.length);
+    
+    for(let i=0; i<maxSize; ++i) {
+        let ai = parseInt(aRevStr[i] || 0);
+        let bi = parseInt(bRevStr[i] || 0);
+        
+        let singleSum = ai + bi + added;
+        if(singleSum < 10) {
+            result += singleSum;
+            added = 0;
+        }
+        else {
+            result += (singleSum % 10);
+            added = Math.floor(singleSum / 10);
+        }
+    }
+    result += added;
+    
+    result = strReverse(result);
+    result = removeInitialZero(result);
+    return result;
+  }
+
+  HugeNum.prototype.sum = function(b) {
+      return sum(this.value, b);
+  }
+
+  function multiply(a, b) {
+    let result = '0';
+    let aString = a.toString();
+    let bString = b.toString();
+    for(let i=aString.length - 1; i>=0; --i) {
+      let localMuliply = '';
       let added = 0;
-      const maxSize = Math.max(aRevStr.length, bRevStr.length);
-      
-      for(let i=0; i<maxSize; ++i) {
-          let ai = parseInt(aRevStr[i] || 0);
-          let bi = parseInt(bRevStr[i] || 0);
-          
-          let singleSum = ai + bi + added;
-          if(singleSum < 10) {
-              result += singleSum;
-              added = 0;
-          }
-          else {
-              result += (singleSum % 10);
-              added = Math.floor(singleSum / 10);
-          }
+      for(let j=0; j<aString.length - 1 - i; ++j)
+        localMuliply += '0';
+      for(let j=bString.length - 1; j>=0; --j) {
+        const ai = parseInt(aString[i]);
+        const bj = parseInt(bString[j]);
+        let m = (ai * bj) + added;
+        added = Math.floor(m / 10);
+        localMuliply += (m % 10);
       }
-      result += added;
-      
-      result = strReverse(result);
-      result = removeInitialZero(result);
-      return result;
+      localMuliply += added;
+      localMuliply = strReverse(localMuliply);
+      localMuliply = removeInitialZero(localMuliply);
+
+      result = sum(result, localMuliply);
+    }
+    return result;
   }
 
   HugeNum.prototype.multiply = function(b) {
-      let result = '0';
-      let aString = this.value.toString();
-      let bString = b.toString();
-      for(let i=aString.length - 1; i>=0; --i) {
-          let localMuliply = '';
-          let added = 0;
-          for(let j=0; j<aString.length - 1 - i; ++j)
-              localMuliply += '0';
-          for(let j=bString.length - 1; j>=0; --j) {
-              const ai = parseInt(aString[i]);
-              const bj = parseInt(bString[j]);
-              let m = (ai * bj) + added;
-              added = Math.floor(m / 10);
-              localMuliply += (m % 10);
-          }
-          localMuliply += added;
-          localMuliply = strReverse(localMuliply);
-          localMuliply = removeInitialZero(localMuliply);
-
-          result = strSum(result, localMuliply);
-      }
-      return result;
+    return multiply(this.value, b);
   }
 
   return new HugeNum;
